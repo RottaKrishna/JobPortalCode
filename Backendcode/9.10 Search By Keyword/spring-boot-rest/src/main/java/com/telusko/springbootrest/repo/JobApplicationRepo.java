e18848d9eea9821142b7775a4fb9dea36f98a803
RottaKrishna <krishna.rotta@nebulogic.com>
2025-04-01 16:44:56 +0530

    Add files via upload

---
+++ Backendcode/9.10 Search By Keyword/spring-boot-rest/src/main/java/com/telusko/springbootrest/repo/JobApplicationRepo.java
@@ -0,0 +1,12 @@
+package com.telusko.springbootrest.repo;
+
+// JobApplicationRepository.java (Repository)
+import com.telusko.springbootrest.model.JobApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JobApplicationRepo extends JpaRepository<JobApplication, Long> {
+
+}
+
